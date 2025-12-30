d84d0585b979363e07ee331bb4e504f243446055
Ikarus-Essays <ikaurs-essays@tutamail.com>
2025-12-30 19:06:08 +0100

    Quartz sync: Dec 30, 2025, 7:06 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -47,6 +47,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.MobileOnly(Component.RecentNotes({title: "Letzte Beiträge:"})),
   ],
 }
 
